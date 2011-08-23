180dd5627cdada5e918d6c801ed325143b5a3bce
Micah Linnemeier <mwlinnem@gmail.com>
2011-08-23 15:29:04 -0400

    tinkered with migration

--- db/migrate/20110821165210_add_username_to_admin.rb
+++ db/migrate/20110821165210_add_username_to_admin.rb
@@ -1,7 +1,6 @@
 class AddUsernameToAdmin < ActiveRecord::Migration
   def self.up
     add_column :admins, :username, :string
-    add_index :admins, :username,              :unique => true
   end
 
   def self.down
