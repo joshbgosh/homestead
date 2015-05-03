ce0f750d45a6f55156654af750e4a01116de8dcb
mlinnem <micah.linnemeier@gmail.com>
2015-05-02 21:06:01 -0500

    Got my start script right (I think) with respect to having a default port (3000) that can be overriden by the global  variable if it's present

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-if [[ -z $PORT ]];
+echo $PORT
+if [[ -z ${PORT} ]]
 	then export PORT=3000;
+else
+	PORT=${PORT}
 fi
-bundle exec rake db:create && bundle exec rake db:migrate && bundle exec rake db:seed && bundle exec jammit && bundle exec rails s -p $PORT
+bundle exec rake db:create && bundle exec rake db:migrate && bundle exec rake db:seed && bundle exec jammit && bundle exec rails s -p ${PORT}
