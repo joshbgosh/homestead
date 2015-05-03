a5e95ec2853cf882d0a05fcf3e8345aa017c0031
mlinnem <micah.linnemeier@gmail.com>
2015-05-02 21:16:56 -0500

    Bash script works locally but not on travis. Added debug messages to try to figure out what's up

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-echo $PORT
+echo "system specified port is ${PORT}"
 if [[ -z ${PORT} ]]
 	then export PORT=3000;
+	echo "assigning default port ${PORT}"
 else
+	echo "using system specified port ${PORT}"
 	PORT=${PORT}
 fi
+echo "PORT variable used is now ${PORT}"
 bundle exec rake db:create && bundle exec rake db:migrate && bundle exec rake db:seed && bundle exec jammit && bundle exec rails s -p ${PORT}
